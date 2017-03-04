8e962d11e86376c1298cfca9d747282f64c7da31
ahfoss <alexanderhfoss@gmail.com>
2017-03-03 23:52:25 -0500

    added functions for creating plus/minus tables; closes #1

---
+++ R/pmTable.R
@@ -0,0 +1,48 @@
+library(Hmisc)
+library(xtable)
+
+makePmTable <- function(
+  means,
+  errs,
+  meanRound = 2,
+  errRound = 2,
+  rowName = rownames(means),
+  colName = colnames(means)
+) {
+  mm <- matrix(
+    paste(
+      format(round(means, meanRound)),
+      '$\\pm$',
+      format(round(errs, errRound)),
+      sep = ''
+    ),
+    nrow = nrow(means),
+    ncol = ncol(means)
+  )
+  dd <- as.data.frame(mm)
+  rownames(dd) <- Hmisc::latexTranslate(rowName)
+  colnames(dd) <- Hmisc::latexTranslate(colName)
+  return(dd)
+}
+
+printPmTable <- function(
+  tab,
+  caption = '',
+  align = rep(c('l','r'), times = c(1,ncol(tab))),
+  sanitize.text.function = function(x) x,
+  size = '\\normalsize',
+  include.rownames = TRUE,
+  ...
+) {
+  xtable::print.xtable(
+    xtable::xtable(
+      tab,
+      align = align,
+      caption = Hmisc::latexTranslate(caption)
+    ),
+    sanitize.text.function = sanitize.text.function,
+    size = size,
+    include.rownames = include.rownames,
+    ...
+  )
+}
